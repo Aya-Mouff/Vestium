37df461c623ec4bff8e5991ce679095c2762910c
Meliani Malak <malak.meliani@ensia.edu.dz>
2025-12-05 23:50:43 +0100

    fixed ui

--- vestium/lib/views/screens/wardrobe/widgets/wardrobe_grid_widget.dart
+++ vestium/lib/views/screens/wardrobe/widgets/wardrobe_grid_widget.dart
@@ -1,35 +1,4 @@
-// // lib/wardrobe_screen/widgets/wardrobe_grid_widget.dart
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import '../cubit/wardrobe_cubit.dart';
-// import '../cubit/wardrobe_state.dart';
-// import 'wardrobe_item_card.dart';
-
-// class WardrobeGridWidget extends StatelessWidget {
-//   const WardrobeGridWidget({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<WardrobeCubit, WardrobeState>(
-//       builder: (context, state) {
-//         return GridView.builder(
-//           padding: const EdgeInsets.symmetric(horizontal: 16),
-//           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//             crossAxisCount: 2,
-//             crossAxisSpacing: 16,
-//             mainAxisSpacing: 16,
-//             childAspectRatio: 0.75,
-//           ),
-//           itemCount: state.items.length,
-//           itemBuilder: (_, index) => WardrobeItemCard(item: state.items[index]),
-//         );
-//       },
-//     );
-//   }
-// }
-
-// ================================================================
-
+// wardrobe_grid_widget.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubit/wardrobe_cubit.dart';
@@ -44,11 +13,12 @@ class WardrobeGridWidget extends StatelessWidget {
     return BlocBuilder<WardrobeCubit, WardrobeState>(
       builder: (context, state) {
         return GridView.builder(
-          padding: const EdgeInsets.all(16), // Reduced from 16
+          // ADD bottom padding here only
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 100),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            crossAxisSpacing: 12, // Reduced from 16
-            mainAxisSpacing: 13, // Reduced from 16
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 13,
             childAspectRatio: 0.46, 
           ),
           itemCount: state.items.length,
